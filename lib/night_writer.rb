a3b769ef561a3cc52d9c1bebc0b826f1553ffb5b
Megan Campbell <megan.campbell26@yahoo.com>
2020-11-07 11:21:54 -0700

    Add file IO beginning

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,16 @@
-class NightWriter
-  
-end
+file = File.open(ARGV[0], "r")
+
+incoming_text = file.read
+
+file.close
+
+capitalized_text = incoming_text.upcase
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(capitalized_text)
+
+writer.close
+
+puts "Created '#{ARGV[1]}' containing #{incoming_text.size} charaters'"
+
