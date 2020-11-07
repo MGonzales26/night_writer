2146e0a0a052f2e7c50f75a519c115747f1beafc
Megan Campbell <megan.campbell26@yahoo.com>
2020-11-07 12:13:23 -0700

    Hide night writer test for now

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,5 @@ require 'simplecov'
 SimpleCov.start
 require 'Minitest/autorun'
 require 'Minitest/pride'
-require './lib/night_writer'
+# require './lib/night_writer'
+require './lib/braille_writer'
