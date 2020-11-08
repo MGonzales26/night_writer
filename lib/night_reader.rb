93337d5bcfe74b38293859f943f31db903872605
Megan Campbell <megan.campbell26@yahoo.com>
2020-11-08 11:51:48 -0700

    Initial commit

---
+++ lib/night_reader.rb
@@ -0,0 +1,20 @@
+require './lib/braille_reader'
+
+file = File.open(ARGV[0], "r")
+
+incoming_text = file.read
+
+document = BrailleReader.new(incoming_text)
+
+english_text = document.translate_text(incoming_text)
+
+file.close
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(english_text)
+
+writer.close
+
+puts "Created '#{ARGV[1]}' containing #{incoming_text.size} charaters'"
+
