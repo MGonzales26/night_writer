dee2835452377323adef01378f891754af5918ba
Megan Campbell <megan.campbell26@yahoo.com>
2020-11-08 12:42:49 -0700

    Add translate letter method and test

--- lib/braille_reader.rb
+++ lib/braille_reader.rb
@@ -1,6 +1,11 @@
 class BrailleReader < Alphabet
+  attr_reader :alphabet
 
   def initialize
     @alphabet = braille_alphabet.invert
+  end
+
+  def translate(text)
+    alphabet[text.split("\n")]
   end
 end
--- test/braille_reader_test.rb
+++ test/braille_reader_test.rb
@@ -15,5 +15,12 @@ class BrailleReaderTest < Minitest::Test
     assert_instance_of BrailleReader, @document
   end
 
-  
+  def test_it_has_inverted_alphabet
+    assert_equal "a", @document.alphabet[["0.", "..", ".."]]
+  end
+
+  def test_it_can_translate_a_letter
+    assert_equal "t", @document.translate(@incoming_text)
+  end
+
 end
